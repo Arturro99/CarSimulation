de598cacaffe460e3bd4563bfc1a4131d35a9290
Wojciech Sowa <dzidadzidowski@gmail.com>
2020-05-22 17:46:22 +0200

    Delete Radio

--- src/warstwaLogiki/pl/exceptions/OutOfFrequencyException.java
+++
@@ -1,9 +0,0 @@
-package warstwaLogiki.pl.exceptions;
-/**
- *
- *  Wyjatek nie pozwalajacy wychodzic poza czestotliwosci radia
- *
- */
-public class OutOfFrequencyException extends Exception {
-    public OutOfFrequencyException(String message) {super(message);}
-}
